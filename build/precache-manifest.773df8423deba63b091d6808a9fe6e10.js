026a32b66529c114e266f7fd58a1c821fd67d97e
stephen wachira <stevohyoung@gmail.com>
2020-01-27 23:22:26 +0300

    great

---
+++ build/precache-manifest.773df8423deba63b091d6808a9fe6e10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626a80e1942dc711fbaa063d5b2e8309",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "b5f202de28b3530266b9",
+    "url": "/portifolio/static/css/main.27097f5f.chunk.css"
+  },
+  {
+    "revision": "a8a1fd62cd34232e88af",
+    "url": "/portifolio/static/js/2.6d0c00e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portifolio/static/js/2.6d0c00e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5f202de28b3530266b9",
+    "url": "/portifolio/static/js/main.d3dd8ac3.chunk.js"
+  },
+  {
+    "revision": "55fe49d961740fc8740f",
+    "url": "/portifolio/static/js/runtime-main.c4feedaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portifolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
